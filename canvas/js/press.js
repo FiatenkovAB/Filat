3d5e805911169e8aadd2cc2aae418f1772058cbd
FiatenkovAB <74141766+FiatenkovAB@users.noreply.github.com>
2021-09-23 12:17:46 +0300

    Add files via upload

---
+++ canvas/js/press.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementByld('c1');
+var ctx = canvas.getContext('2d');
+stx.strokeStyle = "red";
+ctx.moveTo(200, 300);
+ctx.lineTo(150, 150);
+ctx.stroke();
+
+    alert( 'Привет, мир!' );
+
